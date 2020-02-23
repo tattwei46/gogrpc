f5bb2d2a4c98ee449984d91a8e119ce991cae995
DavidCheah <tattwei46@gmail.com>
2020-02-23 15:51:54 +0800

    updated gitignore

--- interceptor/log-interceptor.go
+++ interceptor/log-interceptor.go
@@ -5,25 +5,27 @@ import (
 	"fmt"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/peer"
 	"time"
 )
 
-func LogInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo,
+func ServerLogIntercept(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo,
 	handler grpc.UnaryHandler) (interface{}, error) {
 	today := time.Now()
-	fmt.Println("Accessing product service date: ", today.Format("01/01/2006 15:04:05"))
+	peer, _ := peer.FromContext(ctx)
+	fmt.Printf("Server call from: %s at %s\n", peer.Addr.String(), today.Format("01/01/2006 15:04:05"))
 	fmt.Println("Full method: ", info.FullMethod)
 	return handler(ctx, req)
 }
 
-func ClientLogUnaryInterceptor(ctx context.Context, method string, req interface{}, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+func ClientLogIntercept(ctx context.Context, method string, req interface{}, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
 	today := time.Now()
 	fmt.Println("client call ", method, " service date: ", today.Format("01/01/2006 15:04:05"))
 	err := invoker(ctx, method, req, reply, cc, opts...)
 	return err
 }
 
-func RequestInfoInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo,
+func ServerRequestInfoIntercept(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo,
 	handler grpc.UnaryHandler) (interface{}, error) {
 
 	meta, ok := metadata.FromIncomingContext(ctx)
